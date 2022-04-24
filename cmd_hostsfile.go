4260c445a3abc70f2a284c436e69d3ba97f97a54
jojomi <jojomi.dev@yahoo.com>
2022-04-24 20:10:58 +0200

    Improve hostsfile template

--- cmd_hostsfile.go
+++ cmd_hostsfile.go
@@ -17,26 +17,48 @@ func getCmdHostsfile() *cobra.Command {
 		Short: "Export the network hosts in hostsfile format",
 		Run:   cmdHostsfileHandler,
 	}
+
+	f := cmd.Flags()
+	f.StringP("config", "c", "~/.networkplan/network.yml", "Network config")
+
 	return cmd
 }
 
 func cmdHostsfileHandler(cmd *cobra.Command, args []string) {
+	env := EnvHostsfile{}
+	err := env.ParseFrom(cmd, args)
+	if err != nil {
+		log.Fatal().Err(err).Msg("could not parse params")
+	}
+	handleHostsfile(env)
+}
+
+func handleHostsfile(env EnvHostsfile) {
 	templateData, err := ioutil.ReadFile("templates/hosts")
 	if err != nil {
 		log.Fatal().Err(err).Msg("")
 	}
-	config, err := LoadNetworkConfigFromFile("network.yml")
+	config, err := LoadNetworkConfigFromFile(env.ConfigFilename)
 	if err != nil {
 		log.Fatal().Err(err).Msg("")
 	}
 
 	funcs := template.FuncMap{
 		"join": strings.Join,
+		"newline": func(count ...int) string {
+			var c int
+			if len(count) == 0 {
+				c = 1
+			} else {
+				c = count[0]
+			}
+			return strings.Repeat("\n", c)
+		},
 	}
 	output, err := strtpl.EvalWithFuncMap(string(templateData), funcs, config)
 	if err != nil {
 		log.Fatal().Err(err).Msg("")
 	}
 
-	fmt.Println(output)
+	fmt.Println(strings.TrimSpace(output))
 }
